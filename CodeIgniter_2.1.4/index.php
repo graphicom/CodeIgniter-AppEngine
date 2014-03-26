deacf57eccba8f05deb3bd56c288bdbafae98773
edwardmp <e.poot@evolutionhost.nl>
2014-03-26 22:57:57 +0100

    Bugfixes

--- CodeIgniter_2.1.4/index.php
+++ CodeIgniter_2.1.4/index.php
@@ -1,32 +1,5 @@
 <?php
 
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Provide mod_rewrite like functionality. If a php file in the root directory
-// is explicitely requested then load the file, otherwise load index.php and
-// set get variable 'q' to $_SERVER['REQUEST_URI'].
-if (dirname($path) == '/' && pathinfo($path, PATHINFO_EXTENSION) == 'php') {
-  $file = pathinfo($path, PATHINFO_BASENAME);
-}
-else {
-  $file = 'index.php';
-
-  // Provide mod_rewrite like functionality by using the path which excludes
-  // any other part of the request query (ie. ignores ?foo=bar).
-  $_GET['q'] = $path;
-}
-
-// Override the script name to simulate the behavior without wrapper.php.
-// Ensure that $_SERVER['SCRIPT_NAME'] always begins with a / to be consistent
-// with HTTP request and the value that is normally provided (not what GAE
-// currently provides).
-$_SERVER['SCRIPT_NAME'] = '/' . $file;
-
-// Redirect to controller only if controller is set in URL
-if(isset($_GET['q']) && $_GET['q'] != "//") {
-	header("Location /index.php/" . $_GET['q']);
-}
-
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
@@ -46,7 +19,7 @@ if(isset($_GET['q']) && $_GET['q'] != "//") {
  *
  */
 
-if(isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'],'Google App Engine') !== false)
+if(isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Google App Engine') !== false)
 {
   	define('ENVIRONMENT', 'production');
 }
